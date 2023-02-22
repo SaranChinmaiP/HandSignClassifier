9a7043f1f1d974edc6ff46b06164fe0802550346
Saran P <95578805+SaranChinmaiP@users.noreply.github.com>
2023-02-22 19:11:39 +0530

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -153,7 +153,7 @@ async function loadMobileNetFeatureModel() {
 async function loadModel(){
     console.log("Info : Loading Model - JSON and Weights") ;
      model = await tf.loadLayersModel(
-        'https://saranchinmaip.github.io/HandSignClassifier/myModel/my-model.json');
+        '../myModel/my-model.json');
    model.summary();
    modelLoaded = true ;
    createBars() ;
